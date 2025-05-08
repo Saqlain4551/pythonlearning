0efbdc207bd807b2e74d09993d47bbb59afa8477
Mohd <MohammedSaqlain123@gmail.com>
2025-05-08 17:22:22 +0530

    day-4 code

---
+++ 04string.py
@@ -0,0 +1,84 @@
+print("Thirty" + " " "days" +" " "of" + " " "python")
+print('Coding' + ' ' 'for' + ' ' 'all')
+
+company= ("Coding for all")
+print(company)
+print(len(company))
+
+
+print(company.upper())
+print(company.lower())
+
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+print(company [0:15])
+
+print("Coding" in company)
+
+print(company.replace('Coding' , 'python'))
+
+print("python for every one".replace('everyone' , 'all'))
+
+print(company.split())
+print("google , facebook , apple , IBM , amazon".split(","))
+
+sub_string= 'ing'
+print(company.index(sub_string))
+
+company= ("coding for all")
+print(company)
+print(len(company)- 1)
+
+print(company[10])
+
+print("PFE")
+print("CFA")
+
+sentence= 'you can not end the sentence with because because because is a conjuction '
+print(sentence.index('because')) # print 34
+
+sentence= 'you can not end the sentence with because because because is a conjuction '
+print(sentence.rindex('because'))
+
+sentence= 'you can not end the sentence with because because because is a conjuction '
+print(sentence[33:57])
+
+sentence= "Coding for all"
+print(sentence.startswith('Coding'))
+
+sentence= "Coding for all"
+print(sentence.endswith('Coding'))
+
+Company= "Coding for all"
+print(company.strip())
+
+print('30daysofpython'.isidentifier())
+print('thirty_days_of_python'.isidentifier())
+
+libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('#' .join(libraries))
+
+print('I am enjoying this challenge. \nI just wonder what is next.')
+
+print('name\tage\tcountry\tcity')
+print('saqlain\t19\tIndia\tmumbra')
+
+radius= 10
+area= 3.14 * radius**2
+print(f"The area of the circle with radius {radius} with {area} metre square.")
+
+a = 8
+b= 23
+print(f"{a}+{b} = {a + b}")
+print(f"{a}-{b} = {a - b}")
+print(f"{a}*{b} = {a * b}")
+print(f"{a}**{b} = {a ** b}")
+print(f"{a}/{b} = {a / b}")
+print(f"{a}//{b} = {a // b}")
+print(f"{a}%{b} = {a % b}")
+
+
+
+
