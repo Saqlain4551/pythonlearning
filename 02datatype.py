28afb9458827c812f5923b8abd212c20f380cd4e
Mohd <MohammedSaqlain123@gmail.com>
2025-05-08 17:14:56 +0530

    day-2 code

---
+++ 02datatype.py
@@ -0,0 +1,9 @@
+a= 5   #a is the integer
+
+b= "hello word" # b is the string
+
+c= 7.1 # c is the float
+
+d= True # d is the boolen
+
+e= None # e is the none type variable 
---
+++ 03variable.py
@@ -0,0 +1,67 @@
+first_name= 'Mohd Saqlain'
+last_name= 'Maniyar'
+full_name= 'Mohd Saqlain Javed Ali Maniyar'
+country= 'India'
+Age= 19
+City= 'Mumbra Shilphata'
+is_married= 'false'
+is_true= 'True'
+is_light_on= 'True'
+
+Age,Country,Name,Year= 19 ,'India','Saqlain',2006
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(Age))
+print(type(City))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+
+print(len(first_name)==(last_name))
+
+n1=7
+n2=8
+
+sum=n1+n2
+sub=n2-n1
+product=n1*n2
+division=n1/n2
+floordivision= n1//n2
+remainder=n1%n2
+exp=n1**n2
+
+print(sum)
+print(sub)
+print(product)
+print(division)
+print(floordivision)
+print(remainder)
+print(exp)
+
+radius=30
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+print(area_of_circle)   
+print(circum_of_circle) 
+
+adius= float(input("enter radius of circle: "))
+area_of_circle= 3.14 * radius ** 2
+print(area_of_circle)
+
+first_name= input("enter your first_name: ")
+last_name= input("Enter your last_name: ")
+age= int(input("enter your age: "))
+country= input("enter your country: ")
+
+print( "your fullname is: ", first_name , last_name ) 
+print("how old you are:", age, "year old" )            
+print("where are you from: ", Country) 
+
+help('keyword')
+
+
