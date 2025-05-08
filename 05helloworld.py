2220375b013380ae0a6a03499f9d968f075fc3cc
Mohd <MohammedSaqlain123@gmail.com>
2025-05-08 17:24:04 +0530

    day-1

---
+++ 05helloworld.py
@@ -0,0 +1,53 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3//4)
+print(3%4)
+
+print("Mohammed Saqlain")
+print("Family name ")
+print("India")
+print("I am enjoying a 30 days of task")
+
+print(type(10))
+print(type(13.4))
+print(type([1,2,3,4]))
+print(type(1+4j))
+print(type(['python','java','css']))
+print('your name')
+print("family")
+print("country")
+
+
+x= 10
+print(x)
+print(type(x))
+
+x= 13.4
+print(x)
+print(type(x))
+
+x=1+4j
+print(x)
+print(type(x))
+
+x= True
+print(x)
+print(type(x))
+
+x= 'hello', 'world!'
+print(x)
+print(type(x))
+
+
+
+import math
+x1, y1= 12, 5
+x2, y2= 2, 7
+distance= math.sqrt((x2-x1)**2 + (y2 - y1)**2)
+print(distance)
+
+
+
